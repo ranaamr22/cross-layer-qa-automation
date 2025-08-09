2ecf104bbbdb4b25741c4b39b38e0a3675894ca4
Rana Amr <ranaamr169@yahoo.com>
2025-08-09 22:33:41 +0300

    config: add configurations for api tests

---
+++ jest.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  testEnvironment: 'node',
+  //globalSetup: './globalSetup.js',
+  //globalTeardown: './globalTeardown.js',
+  testTimeout: 30000,
+  reporters: [
+    'default',
+    ['jest-html-reporter', {
+      pageTitle: 'API Test Report',
+      outputPath: './reports/test-report.html',
+      includeFailureMsg: true,
+      includeConsoleLog: true
+    }],
+    ['jest-junit', {
+      outputDirectory: './reports',
+      outputName: 'junit.xml'
+    }]
+  ]
+};
