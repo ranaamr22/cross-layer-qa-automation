8c7fbf5cca293229a8d9ef5faf4a50f1f70e8e9f
Rana Amr <ranaamr169@yahoo.com>
2025-08-11 13:40:44 +0300

    test: update delete all users api testcases
    removing magic strings and add a testcase

--- api-tests/tests/deleteAllUsers.test.js
+++ api-tests/tests/deleteAllUsers.test.js
@@ -4,13 +4,14 @@ require('regenerator-runtime/runtime'); // ensure async/await works
 const PORT = process.env.MOCK_AUTH_PORT || '8080';
 const BASE = `http://localhost:${PORT}`;
 const api = request(BASE);
+const deleteAllUsers_endpoint = '/api/v1/all-users';
 
 
 describe('DELETE ALL USERS', () => {
   
   test('DELETE /api/v1/all-users with valid input → 200 ', async () => {
     const res = await api
-      .delete('/api/v1/all-users')
+      .delete(deleteAllUsers_endpoint)
       .send(
         {
            "key_admin": "keyadmin123"
@@ -24,7 +25,7 @@ describe('DELETE ALL USERS', () => {
 
   test('DELETE /api/v1/all-users with empty input → 400 ', async () => {
     const res = await api
-      .delete('/api/v1/all-users')
+      .delete(deleteAllUsers_endpoint)
       .send(
         {}
       );
@@ -34,7 +35,7 @@ describe('DELETE ALL USERS', () => {
 
   test('DELETE /api/v1/all-users with invalid key → 400 ', async () => {
     const res = await api
-      .delete('/api/v1/all-users')
+      .delete(deleteAllUsers_endpoint)
       .send(
         {
             "admin": "keyadmin123"
@@ -44,9 +45,21 @@ describe('DELETE ALL USERS', () => {
     expect(res.status).toBe(400);
   });
 
+  test('DELETE /api/v1/all-users with invalid Value → 403 ', async () => {
+    const res = await api
+      .delete(deleteAllUsers_endpoint)
+      .send(
+        {
+            "key_admin": "keyadmin"
+        }
+      );
+
+    expect(res.status).toBe(403);
+  });
+
   test('DELETE /api/v1/all-users with empty value → 400 ', async () => {
     const res = await api
-      .delete('/api/v1/all-users')
+      .delete(deleteAllUsers_endpoint)
       .send(
         {
             "key_admin": " "
@@ -54,5 +67,27 @@ describe('DELETE ALL USERS', () => {
       );
 
     expect(res.status).toBe(400);
+  });
+
+  test('DELETE /api/v1/all-users with no users → 200 ', async () => {
+    //delete all users at first
+    await api
+      .delete(deleteAllUsers_endpoint)
+      .send(
+        {
+           "key_admin": "keyadmin123"
+        }
+      );
+
+    //trigger deleteAllUsers end point again 
+    const res = await api
+      .delete(deleteAllUsers_endpoint)
+      .send(
+        {
+            "key_admin": "keyadmin123"
+        }
+    );
+    expect(res.status).toBe(200);
+    expect(res.body.message).toBe('Users deleted with success');
   });
 })
